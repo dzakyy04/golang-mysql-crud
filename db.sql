23c63d230c912998f7c3eecdf863ae8c3508ea17
dzakyy04 <dzakylinggau@gmail.com>
2024-02-29 21:58:32 +0700

    chore(database): add initial database schema in db.sql

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+    nim VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    study_program VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(100) NOT NULL,
+    address VARCHAR(255)
+);
